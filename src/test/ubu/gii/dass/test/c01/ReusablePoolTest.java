73d809bc10f06954293f58af123b9b3a8e9b7f1a
dmh1001 <dmh1001@alu.ubu.es>
2020-02-16 22:49:32 +0100

    consistencia en las pruebas + ReusablePoolTest

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -14,23 +14,50 @@ import ubu.gii.dass.c01.Reusable;
 import ubu.gii.dass.c01.ReusablePool;
 
 /**
- * @author alumno
+ * @author María Ojeda y Daniel Mellado
  *
  */
 public class ReusablePoolTest {
-
+	
+	public final static int TAMANO = 2;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		consistenciaReusablePool();
 	}
-
+	
+	/**
+	 * Método que mantiene la consistencia de la clase ReusablePool para las pruebas.
+	 * 
+	 * Vacia la pila de los elementos que pueda tener y la vuelve a llenar.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	private void consistenciaReusablePool() throws Exception{
+		
+		ReusablePool instancia = ReusablePool.getInstance();
+		try {
+			
+			for(int i = 0; i < TAMANO; i++) {
+				Reusable r = instancia.acquireReusable();
+			}
+				
+		}catch(NotFreeInstanceException e) {
+			
+			for(int i = 0; i < TAMANO; i++) {
+				instancia.releaseReusable(new Reusable());
+			}
+		}
+	}
+ 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	/**
@@ -47,10 +74,9 @@ public class ReusablePoolTest {
 		}
 		
 		ReusablePool instancia2 = ReusablePool.getInstance();
-		
-		
+
 		assert (instancia1 == instancia2);
-		
+	
 	}
 
 	/**
@@ -58,7 +84,20 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testAcquireReusable() {
-		fail("Not yet implemented");
+		
+		ReusablePool instancia = ReusablePool.getInstance();
+		
+		try {
+			
+			for(int i = 0; i <= TAMANO; i++) {
+				Reusable r = instancia.acquireReusable();
+			}
+			
+			assertTrue(false);
+			
+		}catch(NotFreeInstanceException e) {
+			assertTrue(true);		
+		}
 	}
 
 	/**
